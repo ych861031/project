5ff7727bf48359bd11f4491ddc9f9f34cc0a6452
ych861031 <ych861031@gmail.com>
2018-10-12 14:26:34 +0800

    easy compass set

--- app/src/main/java/com/example/yangchunghsuan/project/MainActivity.java
+++ app/src/main/java/com/example/yangchunghsuan/project/MainActivity.java
@@ -6,15 +6,27 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.util.Half.EPSILON;
 
 public class MainActivity extends AppCompatActivity {
 
     SensorManager sensorManager;
     SensorEventListener listener;
-    Sensor sensor;
+    Sensor sensor_m;
+    Sensor sensor_a;
     int count =1;
     TextView textView;
+
+    float[] accelerometerValues=new float[3];
+    float[] magneticFieldValues=new float[3];
+    float[] values=new float[3];
+    float[] r = new float[9];
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +34,46 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        sensor_m = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        sensor_a = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         listener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                if(count++ ==20){
-                    double value = Math.sqrt(event.values[0]*event.values[0] + event.values[1]*event.values[1] +event.values[2]*event.values[2]);
-                    String str = String.format("X:%8.4f , Y:%8.4f , Z:%8.4f \n总值为：%8.4f",event.values[0],event.values[1],event.values[2],value);
-                    count = 1;
-                    textView.setText(str);
 
+                if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+                    accelerometerValues = event.values;
                 }
+                if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD){
+                    magneticFieldValues = event.values;
+                }
+
+
+                //調用getRotaionMatrix獲得變換矩陣R[]
+                SensorManager.getRotationMatrix(r, null, accelerometerValues, magneticFieldValues);
+                SensorManager.getOrientation(r, values);
+
+                //經過SensorManager.getOrientation(R, values);得到的values值爲弧度
+                //轉換爲角度
+                values[0]=(float)Math.toDegrees(values[0]);
+                values[1]=(float)Math.toDegrees(values[1]);
+                values[2]=(float)Math.toDegrees(values[2]);
+
+
+                if(Math.round(values[0])<0){
+                    values[0] = 360+values[0];
+                }
+
+                if (Math.round(values[0])<45 || Math.round(values[0])>315){
+                    textView.setText("北"+ Math.round(values[0]));
+                }else if(Math.round(values[0])>45 && Math.round(values[0])<135){
+                    textView.setText("東"+Math.round(values[0]));
+                }else if(Math.round(values[0])>135 && Math.round(values[0])<225){
+                    textView.setText("南"+ Math.round(values[0]));
+                }else if(Math.round(values[0])>225 && Math.round(values[0])<315){
+                    textView.setText("西" + Math.round(values[0]));
+                }
+
             }
 
             @Override
@@ -44,10 +84,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
     @Override
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(listener,sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listener,sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
